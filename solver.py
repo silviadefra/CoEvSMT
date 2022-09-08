81a03f84928e23e2ad9d34a99f993ac8188cd8c9
Gabriele <yugo.amaril@gmail.com>
2022-09-08 14:34:45 +0200

    Revert "Upload file"
    This reverts commit ee6e0c5470888b976039865787c4072db75303a1.

--- solver.py
+++ solver.py
@@ -1,6 +1,7 @@
 #!/usr/bin python3
 
 from z3 import *
+from multiprocessing import Process
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import numpy as np
 import math
@@ -20,9 +21,9 @@ num_pop=2
 
 
 # Informazioni sulla specifica: possono essere estratte dal file SMT
-num_var=3
+num_var=2
 X=IntVector('x',num_var)
-F=[X[0] > 2, X[2] < 2, X[1] < 10,X[0] + 2*X[1] == 7, X[2] < 10]
+F=[X[0] > 2, X[0] < 2, X[1] < 10]
 F_split=np.array_split(F, num_species)
 d={'formula':F_split, 'vector': [None]*num_species, 'population':[[None]*num_pop]*num_species}
 data=pd.DataFrame(data=d)
@@ -37,19 +38,20 @@ def thread_z3sol(index,s):
         raise ValueError("no solution exists")
     data.at[index,'vector']=[s.model()[i].as_long() for i in X]
     data.at[index,'vector']=[0 if i==None else i for i in data.at[index,'vector']]
-    return 0 # TODO: perché 5?
+    return 5 # TODO: perché 5?
 
-def check_sat(s,index):
+def check_sat(s,index,lock):
     global data
     list_kill_futures=[]
     for i in list(data.index.values):#la lista si modifica? altrimenti prob, comunque prob se legge questa riga e dopo i viene eliminato da altro thread
         if i==index:
             continue
         if s.check([X[j]==data.at[i,'vector'][j] for j in range(num_var)])==sat:
-            s.add(list(data.at[i,'formula']))
-            data.at[index,'formula']=np.append(data.at[index,'formula'],data.at[i,'formula'])
-            data.at[index,'vector']=data.at[i,'vector']
-            data=data.drop(i)
+            with lock:
+                s.add(list(data.at[i,'formula']))
+                data.at[index,'formula']=np.append(data.at[index,'formula'],data.at[i,'formula'])
+                data.at[index,'vector']=data.at[i,'vector']
+                data=data.drop(i)
             list_kill_futures.append(i) #non necessario, basta non riattivare i threads
     return list_kill_futures
 
@@ -63,7 +65,7 @@ def neighbor(index):
     for i in list(data.index.values):
         if i==index:
             continue
-        subtracted = [bin((x ^ y)).count('1') for (x,y) in zip(data.at[index,'vector'],data.at[i,'vector'])]
+        subtracted = [abs(x - y) for (x,y) in zip(data.at[index,'vector'],data.at[i,'vector'])]
         dif[i]=sum(subtracted)
     my_neighbor_dif=min(dif)
     my_neighbor=dif.index(my_neighbor_dif)
@@ -74,19 +76,17 @@ def genetic_alg(index,my_neighbor):
     global data
     generation=0
     best_fitness=my_neighbor[1]
-    data.at[index, 'population']=[data.at[index,'vector']]*num_pop
-    population=list(zip([data.at[index,'vector']]*num_pop,[best_fitness]*num_pop))
+    animal=[format(element1,'b').zfill(16) for element1 in data.at[index,'vector']]
+    data.at[index, 'population']=[animal]*num_pop
+    population=[animal,best_fitness]*num_pop        #data.at[index, 'population']=[animal]*num_pop
     while best_fitness > 0 and generation < 1000:
         new_population = [None]*num_pop
         my_parent1= min(random.sample(population,2), key=lambda individual: individual[1])[0]#my_parent=random.sample(data.at[index, 'population'],2)  #best fitness da fare
         my_neighbor_parent=random.choice(data.at[my_neighbor[0],'population'])
-        my_parent2=[None]*num_var
         if random.random() < 0.7:
-            pos=random.randrange(16)
-            my_bin=f'{my_parent1[i]:016b}'
-            my_neighbor_bin=f'{my_neighbor_parent[i]:016b}'
-            my_parent2[i]=my_bin[:pos]+my_neighbor_bin[pos:]
-            my_parent1[i]=my_neighbor_bin[:pos]+my_bin[pos:]
+            pos=randrange(16)
+            my_parent2=my_parent1[:pos]+my_neighbor_parent[pos:]
+            my_parent1=my_neighbor_parent[:pos]+my_parent1[pos:]
         for i in range(16):
             if random.random()<0.3:
                 #TODO: cambiare bit in pos i
@@ -110,20 +110,19 @@ def run():
                 logging.error(e)
                 executor.shutdown(wait=False, cancel_futures=True)
                 exit()
-        killed=[]
-        for i in range(num_species):
-            if i in killed:
-                continue
-            result=check_sat(solvers[i],i)
-            if not result:
-                killed.append(result)
+        #test da qui in poi
+        lock=Lock()
+        futures=[executor.submit(check_sat,solvers[i],i,lock) for i in range(num_species)]
+        for future in futures:
+            if not future.result():
+                for i in future.result():
+                    futures[i].shutdown(wait=False)
         species_alive=data.index.values
         if len(species_alive)==1:
             print(data.loc[species_alive])
             exit()
         futures=[executor.submit(neighbor,i) for i in list(species_alive)]
-        for future in futures:
-            print(future.result())
+        for future in futures: print(future.result())
 
 
 def main():
